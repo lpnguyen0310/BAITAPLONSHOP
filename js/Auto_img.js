5d735d9ab3e26203f1c9f196e418013df33ba39f
DESKTOP-46K4QJ0\Dell <lp.nguyen0310@gmail.com>
2023-05-03 01:11:32 +0700

    updateHeader_aboutus

--- js/Auto_img.js
+++ js/Auto_img.js
@@ -6,7 +6,7 @@ function slideshow_auto(){
     i++;
     if(i==imgarr.length) i=0;
 }
-setInterval(slideshow_auto,3000);
+setInterval(slideshow_auto,2000);
 
 var imgtag1 = document.getElementById("slideshow1");
 var imgarr1= ["../assets/design_template_img1.1.jpg","../assets/design_template_img1.2.jpg","../assets/design_template_img1.3.jpg","../assets/design_template_img1.4.jpg"];
@@ -26,4 +26,34 @@ function slideshow_auto2(){
     m++;
     if(m==imgarr2.length) m=0;
 }
-setInterval(slideshow_auto2,3000);
+setInterval(slideshow_auto2,2000);
+
+var imgtag3 = document.getElementById("slideshow3");
+var imgarr3= ["../assets/design_template_img3.2.jpg","../assets/design_template_img3.1.jpg","../assets/design_template_img3.3.jpg","../assets/design_template_img3.4.jpg"];
+var t = 0;
+function slideshow_auto3(){
+    imgtag3.setAttribute("src", imgarr3[t]);
+    t++;
+    if(t==imgarr3.length) t=0;
+}
+setInterval(slideshow_auto3,2000);
+
+var imgtag4 = document.getElementById("slideshow4");
+var imgarr4= ["../assets/design_template_img4.1.jpg","../assets/design_template_img4.2.jpg","../assets/design_template_img4.3.jpg","../assets/design_template_img4.4.jpg"];
+var a = 0;
+function slideshow_auto4(){
+    imgtag4.setAttribute("src", imgarr4[a]);
+    a++;
+    if(a==imgarr4.length) a=0;
+}
+setInterval(slideshow_auto4,2000);
+
+var imgtag5 = document.getElementById("slideshow5");
+var imgarr5= ["../assets/design_template_img5.1.jpg","../assets/design_template_img5.2.jpg","../assets/design_template_img5.3.jpg","../assets/design_template_img5.4.jpg"];
+var p = 0;
+function slideshow_auto4(){
+    imgtag5.setAttribute("src", imgarr5[p]);
+    p++;
+    if(p==imgarr5.length) p=0;
+}
+setInterval(slideshow_auto5,2000);
